255f00ebf6c71671e47e71f87b10880160b65ba7
EagleVector <kumarsubhambili3@gmail.com>
2024-03-31 23:31:26 +0530

    Auto config report and springboot hello world

--- src/main/java/com/cherry/spring_school/SpringSchoolApplication.java
+++ src/main/java/com/cherry/spring_school/SpringSchoolApplication.java
@@ -2,12 +2,13 @@ package com.cherry.spring_school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+// exclude bean configuration
+//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @SpringBootApplication
 public class SpringSchoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSchoolApplication.class, args);
 	}
-
 }
