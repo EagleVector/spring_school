3d8b4dad71ce8d79b584c435a31e91e00e8ab61a
EagleVector <kumarsubhambili3@gmail.com>
2024-03-31 22:01:48 +0530

    spring project setup with maven

---
+++ src/main/java/com/cherry/spring_school/SpringSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.spring_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherry/spring_school/SpringSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.spring_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
