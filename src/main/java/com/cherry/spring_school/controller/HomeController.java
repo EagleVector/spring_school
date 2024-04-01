8aec1da6d0742a09690f34ef3d0dcd4ef084e350
EagleVector <kumarsubhambili3@gmail.com>
2024-04-02 01:25:49 +0530

    Home Page Rendering

--- src/main/java/com/cherry/spring_school/controller/HomeController.java
+++ src/main/java/com/cherry/spring_school/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.cherry.spring_school.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = { "", "/", "/home" })
-    public String displayHomePage(Model model) {
-        model.addAttribute("username", "John Doe");
+    public String displayHomePage() {
         return "home.html";
     }
 }
