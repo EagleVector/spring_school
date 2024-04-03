d47c41fc191dc9d559dc9e0dcfca4a7898a61330
EagleVector <kumarsubhambili3@gmail.com>
2024-04-03 22:29:10 +0530

    Session and Application Scope

--- src/main/java/com/cherry/spring_school/service/ContactService.java
+++ src/main/java/com/cherry/spring_school/service/ContactService.java
@@ -5,11 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
-@RequestScope
+//@RequestScope
+@SessionScope
+//@ApplicationScope
 public class ContactService {
 
     private int counter = 0;
