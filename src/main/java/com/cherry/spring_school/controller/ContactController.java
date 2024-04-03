5719c03d12e367b92f3e3dc2e1cde21ab4ba5feb
EagleVector <kumarsubhambili3@gmail.com>
2024-04-03 17:41:07 +0530

    server side validation with jakarta validators with custom errors

--- src/main/java/com/cherry/spring_school/controller/ContactController.java
+++ src/main/java/com/cherry/spring_school/controller/ContactController.java
@@ -1,18 +1,22 @@
 package com.cherry.spring_school.controller;
 import com.cherry.spring_school.model.Contact;
 import com.cherry.spring_school.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-//@Slf4j
+@Slf4j
 @Controller
 public class ContactController {
 
@@ -23,7 +27,8 @@ public class ContactController {
         this.contactService = contactService;
     }
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
@@ -42,8 +47,12 @@ public class ContactController {
 //            return new ModelAndView("redirect:/contact");
 //    }
     @RequestMapping(value = "/saveMsg", method = POST)
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if (errors.hasErrors()){
+            log.error("Contact form validation failed due to: " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
